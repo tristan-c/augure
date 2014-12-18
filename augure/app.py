8623a8e397e0c217d4ac31bc7802c761ee6f1964
Tristan Carranante <tristan@carranante.name>
2014-12-18 15:23:17 +0100

    first commit

---
+++ augure/app.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+import time, os , json, logging
+import schedule, requests
+from envelopes import Envelope, GMailSMTP
+from .daemon3 import daemon as Daemon
+
+class Worker(Daemon):
+    def __init__(self, *args, **kwargs):
+        super(Worker, self).__init__(*args, **kwargs)
+        self.config = None
+
+    def run(self):
+        if not self.config:
+            self.config = self.loadConfiguration()
+
+        self.initLogger(self.config.get("logPath",None))
+
+        self.logger.info("Augure is watching")
+        schedule.every().minute.do(self.check)
+
+        while True:
+            self.logger.debug("check pending job")
+            schedule.run_pending()
+            time.sleep(1)
+
+    def check(self):
+        for url in self.config['urls']:
+            r = requests.get(url)
+            if r.status_code != 200:
+                message = "%s: status %s" % (url,r.status_code)
+                self.logger.warning(message)
+
+                if self.config['emailRecipient']:
+                    self.sendMail(message)
+
+    def sendMail(self,text):
+        self.logger.debug("Building mail")
+
+        envelope = Envelope(
+            from_addr=('augure@augure.nowhere'),
+            to_addr=(self.config['emailRecipient']),
+            subject='Augure has spoken',
+            text_body=text
+        )
+
+        try:
+            self.logger.debug("Sending mail")
+            envelope.send('smtp.googlemail.com', login=self.config["emailLogin"],
+                  password=self.config["emailPassword"], tls=True)
+            self.logger.debug("Mail sent")
+        except Exception as e:
+            self.logger.error(e)
+
+    def loadConfiguration(self, filename=None):
+        configFile = None
+        if filename and os.path.isfile(filename):
+            configFile = filename
+        if os.path.isfile("/etc/augure/augure.conf"):
+            configFile = "/etc/augure/augure.conf"
+
+        homeConfiguration = "%s%s" % (os.path.expanduser("~"),"/.augure/augure.conf")
+        if os.path.isfile(homeConfiguration):
+            configFile = homeConfiguration
+        
+        if not configFile:
+            raise Exception("No Configuration file found")
+
+        try:
+            with open(configFile) as data_file:
+                data = json.load(data_file)
+                self.config = data
+                return data
+        except Exception as e:
+            self.logger.error(e)
+
+
+    def initLogger(self,path="%s%s" % (os.path.expanduser("~"),"/.augure/augure.conf"))
+        logging.basicConfig(
+            format='%(asctime)s %(levelname)s:%(message)s',
+            filename=path,
+            level=logging.INFO
+        )
+        self.logger = logging.getself.logger(__name__)
+
---
+++ setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import sys
+import re
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+if sys.version < '3':
+  import codecs
+  def u(x):
+    return codecs.unicode_escape_decode(x)[0]
+else:
+  def u(x):
+    return x
+
+def get_version():
+    VERSIONFILE = 'augure/__init__.py'
+    initfile_lines = open(VERSIONFILE, 'rt').readlines()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    for line in initfile_lines:
+        mo = re.search(VSRE, line, re.M)
+        if mo:
+            return mo.group(1)
+    raise RuntimeError('Unable to find version string in %s.' % (VERSIONFILE,))
+
+if sys.argv[-1] == 'publish':
+	os.system('python setup.py sdist upload')
+	sys.exit()
+
+setup(
+	name=u('augure'),
+	version=get_version(),
+	description=u('## Set description'),
+	long_description=open('README.rst').read(),
+	license=open('LICENSE').read(),
+	author=u('Tristan Carranante'),
+	author_email=u('tristan@carranante.name'),
+	url='## Set url',
+	keywords=u('## Set keywords'),
+	packages=['augure'],
+    scripts=['scripts/augure'],
+	install_requires=['carotte','docopt'],
+	classifiers=[
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2.6',
+		'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3']
+)
